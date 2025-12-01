5de03cda52f717c55579a76df99bf145d342fbe4
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-01 14:32:07 +0700

    Setup Model, Migration, Controller

--- database/migrations/2025_12_01_061352_create_package_bookings_table.php
+++ database/migrations/2025_12_01_061352_create_package_bookings_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('package_bookings', function (Blueprint $table) {
             $table->id();
+            $table->string('proof');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('package_tour_id')->constrained()->onDelete('cascade');
+            $table->foreignId('package_bank_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('quantity'); // 0 1 2 3
+            $table->unsignedBigInteger('total_amount'); // 0 1 2 3
+            $table->unsignedBigInteger('insurance'); // 0 1 2 3
+            $table->unsignedBigInteger('tax'); // 0 1 2 3
+            $table->unsignedBigInteger('sub_total'); // 0 1 2 3
+            $table->boolean('is_paid'); // true or false
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
