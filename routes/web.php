97617ad109811587185ed81dee579d0d984d4c77
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-03 14:40:46 +0700

    Create Routing for Project

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::middleware('')
+    });
 });
 
 require __DIR__.'/auth.php';
