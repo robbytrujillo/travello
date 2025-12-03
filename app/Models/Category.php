456a062a1e3c711622451a1d000ba2dde1be2cb5
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-03 11:21:47 +0700

    Learn ORM and Fillable Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     //
+    use HasFactory;
+
+    public function tours() {
+        return $this->hasMany(PackageTour::class);
+    }
 }
--- app/Models/PackageBooking.php
+++ app/Models/PackageBooking.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PackageBooking extends Model
 {
     //
+    use HasFactory;
+
+    public function customer() {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function tour() {
+        return $this->belongsTo(PackageTour::class);
+    }
+    
+    public function bank() {
+        return $this->belongsTo(PackageBank::class);
+    }
+
+
 }
