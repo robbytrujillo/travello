8b7ac74eb327603ec5b6854cecddd05024c8b2bc
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-03 11:33:55 +0700

    Learn ORM and Fillable Model

--- app/Models/PackageTour.php
+++ app/Models/PackageTour.php
@@ -9,4 +9,12 @@ class PackageTour extends Model
 {
     //
     use HasFactory;
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function package_photos() {
+        return $this->hasMany(PackagePhoto::class);
+    } 
 }
