81b15951d6cbc1f8d1cbfd668d42085c15ab0f46
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-03 13:39:40 +0700

    Learn ORM and Fillable Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,11 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
     //
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'icon',
+        'slug',
+    ];
 
     public function tours() {
         return $this->hasMany(PackageTour::class);
