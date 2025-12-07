d2bf6292e34c25bff8523adc51dadd4338b4a910
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-07 09:48:14 +0700

    Preparation of View List Data Category

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -1,31 +1,31 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('New Category') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg"> 
-                <form method="POST" action=" " enctype="multipart/form-data">
+            <div class="p-10 overflow-hidden bg-white shadow-sm sm:rounded-lg"> 
+                <form method="POST" action="{{ route('admin.categories.store') }}" enctype="multipart/form-data">
                     @csrf
 
                     <div>
                         <x-input-label for="name" :value="__('Name')" />
-                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                        <x-text-input id="name" class="block w-full mt-1" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
                         <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
                         <x-input-label for="icon" :value="__('icon')" />
-                        <x-text-input id="icon" class="block mt-1 w-full" type="file" name="icon" required autofocus autocomplete="icon" />
+                        <x-text-input id="icon" class="block w-full mt-1" type="file" name="icon" required autofocus autocomplete="icon" />
                         <x-input-error :messages="$errors->get('icon')" class="mt-2" />
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
             
-                        <button type="submit" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                        <button type="submit" class="px-6 py-4 font-bold text-white bg-indigo-700 rounded-full">
                             Add New Category
                         </button>
                     </div>
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,46 +1,48 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex flex-row justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <div class="flex flex-row items-center justify-between">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 {{ __('Manage Categories') }}
             </h2>
-            <a href=" " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+            <a href="{{ route('admin.categories.create') }}" class="px-6 py-4 font-bold text-white bg-indigo-700 rounded-full">
                 Add New
             </a>
         </div>
     </x-slot>
     
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="flex flex-col p-10 overflow-hidden bg-white shadow-sm sm:rounded-lg gap-y-5">
 
-             
-                <div class="item-card flex flex-row justify-between items-center">
+             @forelse ($categories as $category)
+                <div class="flex flex-row items-center justify-between item-card">
                     <div class="flex flex-row items-center gap-x-3">
-                        <img src=" " alt="" class="rounded-2xl object-cover w-[90px] h-[90px]">
+                        <img src="{{ Storage::url('') }}" alt="" class="rounded-2xl object-cover w-[90px] h-[90px]">
                         <div class="flex flex-col">
-                            <h3 class="text-indigo-950 text-xl font-bold">cate name</h3>
+                            <h3 class="text-xl font-bold text-indigo-950">cate name</h3>
                         </div>
                     </div> 
-                    <div  class="hidden md:flex flex-col">
-                        <p class="text-slate-500 text-sm">Date</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">date</h3>
+                    <div  class="flex-col hidden md:flex">
+                        <p class="text-sm text-slate-500">Date</p>
+                        <h3 class="text-xl font-bold text-indigo-950">date</h3>
                     </div>
-                    <div class="hidden md:flex flex-row items-center gap-x-3">
-                        <a href=" " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                    <div class="flex-row items-center hidden md:flex gap-x-3">
+                        <a href=" " class="px-6 py-4 font-bold text-white bg-indigo-700 rounded-full">
                             Edit
                         </a>
                         <form action=" " method="POST">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="font-bold py-4 px-6 bg-red-700 text-white rounded-full">
+                            <button type="submit" class="px-6 py-4 font-bold text-white bg-red-700 rounded-full">
                                 Delete
                             </button>
                         </form>
                     </div>
                 </div>
+             @empty
+                 <p>There is no new category data yet</p>
+             @endforelse
                 
-
             </div>
         </div>
     </div>
