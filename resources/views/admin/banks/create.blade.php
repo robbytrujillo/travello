4cf2419d3dee7b60a6322fe24d36e9e2de2a8f78
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-12 23:55:31 +0700

    Crud Package Bank

--- resources/views/admin/banks/create.blade.php
+++ resources/views/admin/banks/create.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('New Bank') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg">
+            <div class="p-10 overflow-hidden bg-white shadow-sm sm:rounded-lg">
 
                  
                 
@@ -15,32 +15,32 @@
                     @csrf
 
                     <div>
-                        <x-input-label for="bank_name" :value="__('bank_name')" />
-                        <x-text-input id="bank_name" class="block mt-1 w-full" type="text" name="bank_name" :value="old('bank_name')" required autofocus autocomplete="name" />
+                        <x-input-label for="bank_name" :value="__('Bank Name')" />
+                        <x-text-input id="bank_name" class="block w-full mt-1" type="text" name="bank_name" :value="old('bank_name')" required autofocus autocomplete="name" />
                         <x-input-error :messages="$errors->get('bank_name')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
-                        <x-input-label for="bank_account_name" :value="__('bank_account_name')" />
-                        <x-text-input id="bank_account_name" class="block mt-1 w-full" type="text" name="bank_account_name" :value="old('bank_account_name')" required autofocus autocomplete="name" />
+                        <x-input-label for="bank_account_name" :value="__('Bank Account Name')" />
+                        <x-text-input id="bank_account_name" class="block w-full mt-1" type="text" name="bank_account_name" :value="old('bank_account_name')" required autofocus autocomplete="name" />
                         <x-input-error :messages="$errors->get('bank_account_name')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
-                        <x-input-label for="bank_account_number" :value="__('bank_account_number')" />
-                        <x-text-input id="bank_account_number" class="block mt-1 w-full" type="number" name="bank_account_number" :value="old('bank_account_number')" required autofocus autocomplete="name" />
+                        <x-input-label for="bank_account_number" :value="__('Bank Account Number')" />
+                        <x-text-input id="bank_account_number" class="block w-full mt-1" type="number" name="bank_account_number" :value="old('bank_account_number')" required autofocus autocomplete="name" />
                         <x-input-error :messages="$errors->get('bank_account_number')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
-                        <x-input-label for="logo" :value="__('logo')" />
-                        <x-text-input id="logo" class="block mt-1 w-full" type="file" name="logo" required autofocus autocomplete="logo" />
+                        <x-input-label for="logo" :value="__('Logo')" />
+                        <x-text-input id="logo" class="block w-full mt-1" type="file" name="logo" required autofocus autocomplete="logo" />
                         <x-input-error :messages="$errors->get('logo')" class="mt-2" />
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
             
-                        <button type="submit" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                        <button type="submit" class="px-6 py-4 font-bold text-white bg-indigo-700 rounded-full">
                             Add New Bank
                         </button>
                     </div>
