af2827482342bbb161c30ddcb7ae2326a9b864b0
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-14 04:23:30 +0700

    View Data Tour Feature

--- resources/views/admin/package_tours/index.blade.php
+++ resources/views/admin/package_tours/index.blade.php
@@ -1,43 +1,47 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex flex-row justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <div class="flex flex-row items-center justify-between">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 {{ __('Manage Tours') }}
             </h2>
-            <a href="#" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+            <a href="{{ route('admin.package_tours.create') }}" class="px-6 py-4 font-bold text-white bg-indigo-700 rounded-full">
                 Add New
             </a>
         </div>
     </x-slot>
     
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="flex flex-col p-10 overflow-hidden bg-white shadow-sm sm:rounded-lg gap-y-5">
 
-
-                <div class="item-card flex flex-row justify-between items-center">
-                    <div class="flex flex-row items-center gap-x-3">
-                        <img src="#" alt="" class="rounded-2xl object-cover w-[120px] h-[90px]">
-                        <div class="flex flex-col">
-                            <h3 class="text-indigo-950 text-xl font-bold">tour name</h3>
-                        <p class="text-slate-500 text-sm">tour category</p>
+                @forelse($package_tours as $tour)
+                    <div class="flex flex-row items-center justify-between item-card">
+                        <div class="flex flex-row items-center gap-x-3">
+                            <img src="#" alt="" class="rounded-2xl object-cover w-[120px] h-[90px]">
+                            <div class="flex flex-col">
+                                <h3 class="text-xl font-bold text-indigo-950">tour name</h3>
+                            <p class="text-sm text-slate-500">tour category</p>
+                            </div>
+                        </div> 
+                        <div  class="flex-col hidden md:flex">
+                            <p class="text-sm text-slate-500">Price</p>
+                            <h3 class="text-xl font-bold text-indigo-950">Rp 0</h3>
                         </div>
-                    </div> 
-                    <div  class="hidden md:flex flex-col">
-                        <p class="text-slate-500 text-sm">Price</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">Rp 0</h3>
+                        <div  class="flex-col hidden md:flex">
+                            <p class="text-sm text-slate-500">Total Days</p>
+                            <h3 class="text-xl font-bold text-indigo-950">11 Days</h3>
+                        </div>
+                        <div class="flex-row items-center hidden md:flex gap-x-3">
+                            <a href="#" class="px-6 py-4 font-bold text-white bg-indigo-700 rounded-full">
+                                Manage
+                            </a>
+                        </div>
                     </div>
-                    <div  class="hidden md:flex flex-col">
-                        <p class="text-slate-500 text-sm">Total Days</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">11 Days</h3>
-                    </div>
-                    <div class="hidden md:flex flex-row items-center gap-x-3">
-                        <a href="#" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
-                            Manage
-                        </a>
-                    </div>
-                </div>
-                
+                @empty
+                    <p>
+                        There is no new tour data yet
+                    </p>
+                @endforelse
 
             </div>
         </div>
