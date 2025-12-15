849fd5fd2940af234127458f770bbc15d66ad7bd
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-15 15:09:24 +0700

    Register Page Integration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,69 +1,3 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block w-full mt-1" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-        
-        <!-- Phone Number -->
-        <div class="mt-4">
-            <x-input-label for="phone_number" :value="__('Phone Number')" />
-            <x-text-input id="phone_number" class="block w-full mt-1" type="number" name="phone_number" :value="old('phone_number')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('phone_number')" class="mt-2" />
-        </div>
-        
-        <!-- Avatar -->
-        <div class="mt-4">
-            <x-input-label for="avatar" :value="__('Avatar')" />
-            <x-text-input id="avatar" class="block w-full mt-1" type="file" name="avatar" :value="old('avatar')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('avatar')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block w-full mt-1"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block w-full mt-1"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
 
 <!doctype html>
 <html>
@@ -79,7 +13,8 @@
           <div class="w-[calc(100%-26px)] rounded-[20px] overflow-hidden relative">
             <img src="assets/backgrounds/Asset-signup.png" class="object-contain w-full h-full" alt="background">
           </div>
-          <form action="home.html" class="flex flex-col w-full bg-white p-[24px_16px] gap-8 rounded-[22px] items-center">
+          <form method="POST" action="{{ route('register') }}" class="flex flex-col w-full bg-white p-[24px_16px] gap-8 rounded-[22px] items-center" enctype="multipart/form-data">
+            @csrf
             <div class="flex flex-col gap-1 text-center">
               <h1 class="font-semibold text-2xl leading-[42px] ">Sign Up</h1>
               <p class="text-sm leading-[25px] tracking-[0.6px] text-darkGrey">Enter valid data to create your account</p>
@@ -99,7 +34,7 @@
                         <span id="fileName" class="text-sm tracking-035 leading-[22px] text-nowrap"></span>
                       </div>
                     </div>
-                    <input type="file" name="file" id="file" class="hidden">
+                    <input type="file" name="avatar" id="file" class="hidden">
                   </button>
                 </div>
               </div>
@@ -109,7 +44,7 @@
                   <div class="flex w-4 h-4 shrink-0">
                     <img src="assets/icons/user-flat-black.svg" alt="icon">
                   </div>
-                  <input type="text" class="appearance-none outline-none w-full text-sm placeholder:text-[#BFBFBF] tracking-[0.35px]" placeholder="Write your full name">
+                  <input type="text" name="name" class="appearance-none outline-none w-full text-sm placeholder:text-[#BFBFBF] tracking-[0.35px]" placeholder="Write your full name">
                 </div>
               </div>
               <div class="flex flex-col w-full gap-1">
@@ -118,7 +53,7 @@
                   <div class="flex w-4 h-4 shrink-0">
                     <img src="assets/icons/call.svg" alt="icon">
                   </div>
-                  <input type="tel" class="appearance-none outline-none w-full text-sm placeholder:text-[#BFBFBF] tracking-[0.35px]" placeholder="Your valid phone number">
+                  <input type="tel" name="phone_number" class="appearance-none outline-none w-full text-sm placeholder:text-[#BFBFBF] tracking-[0.35px]" placeholder="Your valid phone number">
                 </div>
               </div>
               <div class="flex flex-col w-full gap-1">
@@ -127,7 +62,7 @@
                   <div class="flex w-4 h-4 shrink-0">
                     <img src="assets/icons/sms.svg" alt="icon">
                   </div>
-                  <input type="email" class="appearance-none outline-none w-full text-sm placeholder:text-[#BFBFBF] tracking-[0.35px]" placeholder="Your email address">
+                  <input type="email" name="email" class="appearance-none outline-none w-full text-sm placeholder:text-[#BFBFBF] tracking-[0.35px]" placeholder="Your email address">
                 </div>
               </div>
               <div class="flex flex-col w-full gap-1">
@@ -136,7 +71,7 @@
                   <div class="flex w-4 h-4 shrink-0">
                     <img src="assets/icons/password-lock.svg" alt="icon">
                   </div>
-                  <input type="password" id="password" name="" class="appearance-none outline-none w-full text-sm placeholder:text-[#BFBFBF] tracking-[0.35px]" placeholder="Enter your valid password">
+                  <input type="password" id="password" name="password" class="appearance-none outline-none w-full text-sm placeholder:text-[#BFBFBF] tracking-[0.35px]" placeholder="Enter your valid password">
                   <button type="button" class="flex w-4 h-4 reveal-password shrink-0" onclick="togglePasswordVisibility('password', this)">
                     <img src="assets/icons/password-eye.svg" class="see-password" alt="icon">
                     <img src="assets/icons/password-eye-slash.svg" class="hidden hide-password" alt="icon">
@@ -149,7 +84,7 @@
                   <div class="flex w-4 h-4 shrink-0">
                     <img src="assets/icons/password-lock.svg" alt="icon">
                   </div>
-                  <input type="password" id="confirm-password" name="" class="appearance-none outline-none w-full text-sm placeholder:text-[#BFBFBF] tracking-[0.35px]" placeholder="Confirm your valid password">
+                  <input type="password" id="confirm-password" name="password_confirmation" class="appearance-none outline-none w-full text-sm placeholder:text-[#BFBFBF] tracking-[0.35px]" placeholder="Confirm your valid password">
                   <button type="button" class="flex w-4 h-4 reveal-password shrink-0" onclick="togglePasswordVisibility('confirm-password', this)">
                     <img src="assets/icons/password-eye.svg" class="see-password" alt="icon">
                     <img src="assets/icons/password-eye-slash.svg" class="hidden hide-password" alt="icon">
@@ -158,7 +93,7 @@
               </div>
             </div>
             <button type="submit" class="bg-[#4D73FF] p-[16px_24px] w-full max-w-[311px] rounded-[10px] text-center text-white font-semibold hover:bg-[#06C755] transition-all duration-300">Sign up</button>
-            <p class="text-sm text-center tracking-035 text-darkGrey">Already have account? <a href="index.html" class="text-[#4D73FF] font-semibold tracking-[0.6px]">Sign In</a></p>
+            <p class="text-sm text-center tracking-035 text-darkGrey">Already have account? <a href="{{ route('login') }}" class="text-[#4D73FF] font-semibold tracking-[0.6px]">Sign In</a></p>
           </form>
         </div>
     </section>
