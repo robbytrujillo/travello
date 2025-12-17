4c5ea9b231bfec8664ef5e2ca0fb3fe1d2aa5acf
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-17 23:05:09 +0700

    Booking New Tour Feature

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\PackageBank;
 use App\Models\PackageTour;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StorePackageTourRequest;
